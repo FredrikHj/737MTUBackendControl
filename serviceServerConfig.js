268e8d2c898e38bb3a2aef8890d7a9e69483d1c4
Fredrik Hjärpe <fredrik.hjarpe@outlook.com>
2024-01-26 14:07:47 +0100

    Backup¨

---
+++ serviceServerConfig.js
@@ -0,0 +1,12 @@
+var serviceServerConfig = {
+    fsuipc:  {
+        name: "fsuipc",
+        hostname: "localhost",
+        port:  2048,
+    },
+    phidgets: {
+        hostname: "hub5000.local.",
+        port:  5661,
+    },
+};
+export default serviceServerConfig;
